d2a997a8d357a97df3d823febe256144b68a685a
JEHFxD <148361405+JEHFxD@users.noreply.github.com>
2024-06-13 17:16:25 -0500

    Add files via upload

---
+++ CicloWhile/src/main/java/whilecicle/Ejercicio2.java
@@ -0,0 +1,11 @@
+package whilecicle;
+
+public class Ejercicio2 {
+    public static void main(String[] args) {
+        int i = 2;
+        while (i <= 10) {
+            System.out.println(i);
+            i += 2;
+        }
+    }
+}
---
+++ CicloWhile/src/main/java/whilecicle/Ejercicio3.java
@@ -0,0 +1,11 @@
+package whilecicle;
+
+public class Ejercicio3 {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            System.out.println("Hola");
+            i++;
+        }
+    }
+}
